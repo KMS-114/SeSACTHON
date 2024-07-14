f3ede6261bd237a858f2ffe205d20cbec1a4d661
drepion43 <drepion43@gmail.com>
2024-07-14 15:40:14 +0900

    0713_1
    업데이트

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # SeSACTHON
 
-### Start Server
-cmd
+### Node.js Install
+Node.js
+conda install nodejs
 
-[uvicorn main:app --reload]
+### Svelte Start
+npm create vite@latest frontend -- --template svelte
+cd frontend
+npm install
+npm install svelte-routing
+npm install less less-loader --save-dev
+npx lessc src/styles/login.less src/styles/styles.css
+npm install bootstrap@5.3.3
+npm run dev
+
+### Fastapi Start
+cmd
+uvicorn main:app --reload
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+import Backend.models
+from Backend.database import engine
+
+# 자동 테이블 생성
+Backend.models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",    # 또는 "http://localhost:5173"
+]
 
-@app.get("/hello")
-def hello():
-    return {"message": "hello world"}
+from Backend.domain.board import board_router
+from Backend.domain.user import user_router
 
+app.include_router(board_router.app, tags=["board"])
+app.include_router(user_router.app, tags=["user"])
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
