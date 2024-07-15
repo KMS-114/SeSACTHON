780362c0b3aa9529203b66062020108b2eb89c5e
양호준 <hojune0303@naver.com>
2024-07-15 21:01:15 +0900

    양호준 [제목] Job Posting DB의 업데이트 관련 API, CRUD 구현

--- Backend/app/api/job_posting/repository.py
+++ Backend/app/api/job_posting/repository.py
@@ -1,21 +1,49 @@
 from .schema import JobPostingModel, JobPostingCollection
-from ...database import mongodb
+from bson import ObjectId
+from ...database import mongodb, collection
 
+async def update_description(job_posting_id:str, description:str)->dict:
+    job_posting = await collection.find_one({"_id": ObjectId(job_posting_id)})
+    if job_posting is None:
+        return {"message": "job_posting_id가 유효하지 않습니다."}
 
+    result = await collection.update_one({"_id": ObjectId(job_posting_id)}, {"$set": {f"description": description}})
+    if result.modified_count == 1:
+        return {f"message": "description update 완료"}
+    
+async def update_cover_letter_question(job_posting_id:str, content:str, char_limit:int)->dict:
+    job_posting = await collection.find_one({"_id": ObjectId(job_posting_id)})
+    if job_posting is None:
+        return {"message": "job_posting_id가 유효하지 않습니다."}
+
+    result = await collection.update_one({"_id": ObjectId(job_posting_id)}, {"$push": {f"coverLetterQuestions": {"content": content, "charLimit": char_limit}}})
+    if result.modified_count == 1:
+        return {f"message": "cover_letter_question update 완료"}
+
+async def update_qualification(job_posting_id:str, key:str, value:str):
+    job_posting = await collection.find_one({"_id": ObjectId(job_posting_id)})
+    if job_posting is None:
+        return {"message": "job_posting_id가 유효하지 않습니다."}
+
+    result = await collection.update_one({"_id": ObjectId(job_posting_id)}, {"$set": {f"qualificationsRequired.customQualification.{key}": value}})
+    if result.modified_count == 1:
+        return {f"message": "custom_qualification update 완료"}
+    
 async def create_job_posting(
     job_posting: JobPostingModel
 ) -> JobPostingModel:
-    collection = mongodb.get_collection("jobPosting")
+    
     new_job_posting = await collection.insert_one(
         job_posting.model_dump(by_alias=True, exclude=["id"])
     )
     created_job_posting = await collection.find_one(
         {"_id": new_job_posting.inserted_id}
     )
+
     return created_job_posting
 
 
 async def get_job_postings():
-    collection = mongodb.get_collection("jobPosting")
+    # collection = mongodb.get_collection("job_posting")
     list_job_postings = await collection.find().to_list(length=10)
     return JobPostingCollection(jobPostings=list_job_postings)
--- Backend/app/api/job_posting/router.py
+++ Backend/app/api/job_posting/router.py
@@ -1,14 +1,28 @@
 from fastapi import APIRouter
 
-from .schema import JobPostingModel, JobPostingCollection
-from .repository import get_job_postings
-
+from .schema import JobPostingModel, JobPostingCollection, CoverLetterQuestion, QualificationRequired
+from .repository import get_job_postings, create_job_posting, update_cover_letter_question, update_qualification, update_description
+from ...database import collection
+from bson import ObjectId
 
 router = APIRouter(prefix="/job_posting")
 
+@router.put("/description")
+async def api_description(job_posting_id:str, description:str):
+    return await update_description(job_posting_id, description)
 
-# async def create_job_posting():
+@router.put("/cover_letter_question/")
+async def api_cover_letter_question(job_posting_id:str, content:str, char_limit:int):
+    return await update_cover_letter_question(job_posting_id, content, char_limit)
+    
+@router.put("/qualification/")
+async def api_update_qualification(job_posting_id:str, key:str, value:str):
+    return await update_qualification(job_posting_id, key, value)
 
+@router.post("/jop_posting")
+async def api_job_posting(job_posting:JobPostingModel):
+    job_posting_dict = await create_job_posting(job_posting)
+    return {"message": "jobposting created successfully"}
 
 @router.get(
     "/all",
